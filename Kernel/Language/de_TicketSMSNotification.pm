96b59b1966fd0fd5e65eeab30a57d05a7a7506d3
reneeb <github@renee-baecker.de>
2014-07-18 08:27:55 +0200

    add base translation file

---
+++ Kernel/Language/de_TicketSMSNotification.pm
@@ -0,0 +1,27 @@
+# --
+# Kernel/Language/de_TicketSMSNotification.pm - German translations for TicketSMSNotification
+# Copyright (C) 2014 Perl-Services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::de_TicketSMSNotification;
+
+use strict;
+use warnings;
+
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation} || {};
+
+    $Lang->{} = '';
+
+    return 1;
+}
+
+1;
